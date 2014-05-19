c0258582fa5dec620e2aee072d44722850fdcce2
rritoch <rritoch@gmail.com>
2014-05-19 23:05:21 +0800

    Repaired NVML library reflection

--- src/com/vnetpublishing/nvml/util.clj
+++ src/com/vnetpublishing/nvml/util.clj
@@ -7,23 +7,23 @@
   [error_code]
   (let [c (Class/forName "com.vnetpublishing.swig.nvml.nvmlReturn_t")]
   (cond
-    (= error_code (.get (.getField c "NVML_SUCCESS"))) "NVML_SUCCESS"
-    (= error_code (.get (.getField c "NVML_ERROR_UNINITIALIZED"))) "NVML_ERROR_UNINITIALIZED"
-    (= error_code (.get (.getField c "NVML_ERROR_INVALID_ARGUMENT"))) "NVML_ERROR_INVALID_ARGUMENT"
-    (= error_code (.get (.getField c "NVML_ERROR_NOT_SUPPORTED"))) "NVML_ERROR_NOT_SUPPORTED"
-    (= error_code (.get (.getField c "NVML_ERROR_NO_PERMISSION"))) "NVML_ERROR_NO_PERMISSION"
-    (= error_code (.get (.getField c "NVML_ERROR_ALREADY_INITIALIZED"))) "NVML_ERROR_ALREADY_INITIALIZED"
-    (= error_code (.get (.getField c "NVML_ERROR_NOT_FOUND"))) "NVML_ERROR_NOT_FOUND"
-    (= error_code (.get (.getField c "NVML_ERROR_INSUFFICIENT_SIZE"))) "NVML_ERROR_INSUFFICIENT_SIZE"
-    (= error_code (.get (.getField c "NVML_ERROR_INSUFFICIENT_POWER"))) "NVML_ERROR_INSUFFICIENT_POWER"
-    (= error_code (.get (.getField c "NVML_ERROR_DRIVER_NOT_LOADED"))) "NVML_ERROR_DRIVER_NOT_LOADED"
-    (= error_code (.get (.getField c "NVML_ERROR_TIMEOUT"))) "NVML_ERROR_TIMEOUT"
-    (= error_code (.get (.getField c "NVML_ERROR_IRQ_ISSUE"))) "NVML_ERROR_IRQ_ISSUE"
-    (= error_code (.get (.getField c "NVML_ERROR_LIBRARY_NOT_FOUND"))) "NVML_ERROR_LIBRARY_NOT_FOUND"
-    (= error_code (.get (.getField c "NVML_ERROR_FUNCTION_NOT_FOUND"))) "NVML_ERROR_FUNCTION_NOT_FOUND"
-    (= error_code (.get (.getField c "NVML_ERROR_CORRUPTED_INFOROM"))) "NVML_ERROR_CORRUPTED_INFOROM"
-    (= error_code (.get (.getField c "NVML_ERROR_GPU_IS_LOST"))) "NVML_ERROR_GPU_IS_LOST"
-    (= error_code (.get (.getField c "NVML_ERROR_UNKNOWN"))) "NVML_ERROR_UNKNOWN"
+    (= error_code (.get (.getField c "NVML_SUCCESS") nil)) "NVML_SUCCESS"
+    (= error_code (.get (.getField c "NVML_ERROR_UNINITIALIZED") nil)) "NVML_ERROR_UNINITIALIZED"
+    (= error_code (.get (.getField c "NVML_ERROR_INVALID_ARGUMENT") nil)) "NVML_ERROR_INVALID_ARGUMENT"
+    (= error_code (.get (.getField c "NVML_ERROR_NOT_SUPPORTED") nil)) "NVML_ERROR_NOT_SUPPORTED"
+    (= error_code (.get (.getField c "NVML_ERROR_NO_PERMISSION") nil)) "NVML_ERROR_NO_PERMISSION"
+    (= error_code (.get (.getField c "NVML_ERROR_ALREADY_INITIALIZED") nil)) "NVML_ERROR_ALREADY_INITIALIZED"
+    (= error_code (.get (.getField c "NVML_ERROR_NOT_FOUND") nil)) "NVML_ERROR_NOT_FOUND"
+    (= error_code (.get (.getField c "NVML_ERROR_INSUFFICIENT_SIZE") nil)) "NVML_ERROR_INSUFFICIENT_SIZE"
+    (= error_code (.get (.getField c "NVML_ERROR_INSUFFICIENT_POWER") nil)) "NVML_ERROR_INSUFFICIENT_POWER"
+    (= error_code (.get (.getField c "NVML_ERROR_DRIVER_NOT_LOADED") nil)) "NVML_ERROR_DRIVER_NOT_LOADED"
+    (= error_code (.get (.getField c "NVML_ERROR_TIMEOUT") nil)) "NVML_ERROR_TIMEOUT"
+    (= error_code (.get (.getField c "NVML_ERROR_IRQ_ISSUE") nil)) "NVML_ERROR_IRQ_ISSUE"
+    (= error_code (.get (.getField c "NVML_ERROR_LIBRARY_NOT_FOUND") nil)) "NVML_ERROR_LIBRARY_NOT_FOUND"
+    (= error_code (.get (.getField c "NVML_ERROR_FUNCTION_NOT_FOUND") nil)) "NVML_ERROR_FUNCTION_NOT_FOUND"
+    (= error_code (.get (.getField c "NVML_ERROR_CORRUPTED_INFOROM") nil)) "NVML_ERROR_CORRUPTED_INFOROM"
+    (= error_code (.get (.getField c "NVML_ERROR_GPU_IS_LOST") nil)) "NVML_ERROR_GPU_IS_LOST"
+    (= error_code (.get (.getField c "NVML_ERROR_UNKNOWN") nil)) "NVML_ERROR_UNKNOWN"
     :else (str "UNKNOWN_ERROR (" error_code ")")
   )
   )
@@ -31,8 +31,8 @@
 
 (defn assert-success [f result]
   (let [c (Class/forName "com.vnetpublishing.swig.nvml.nvmlReturn_t")]
-    (if (= result (.get (.getField c "NVML_SUCCESS")))
-      (.get (.getField c "NVML_SUCCESS"))
+    (if (= result (.get (.getField c "NVML_SUCCESS") nil))
+      (.get (.getField c "NVML_SUCCESS") nil)
       (throw (Exception. (str "Error: " (strerror result) " in call to " f)))
     )
   )
